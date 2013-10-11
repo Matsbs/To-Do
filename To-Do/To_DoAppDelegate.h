15e91c825156be9f2268b62ceafe9175a22c2837
Mats Sandvoll <matssandvoll@gmail.com>
2013-10-11 12:53:57 +0100

    Initial Commit

---
+++ To-Do/To_DoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  To_DoAppDelegate.h
+//  To-Do
+//
+//  Created by Mats Sandvoll on 11.10.13.
+//  Copyright (c) 2013 Mats Sandvoll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface To_DoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ To-Do/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  To-Do
+//
+//  Created by Mats Sandvoll on 11.10.13.
+//  Copyright (c) 2013 Mats Sandvoll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "To_DoAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([To_DoAppDelegate class]));
+    }
+}
